9f4da82dee2e5acc8cefc56908ffb6cd36477318
morumotto <saacostad@unal.edu.co>
2023-09-17 12:31:09 -0500

    Espectrometria atomica

---
+++ Espectrometría.R
@@ -0,0 +1,9 @@
+mercury_data = list(
+  ang=list(7, 8, 9, 10, 10, 15, 17, 19, 20, 20, 7, 8, 9, 10, 10, 15, 17, 19, 20, 20),
+  min=list(35, 36, 33, 5, 7, 20, 23, 22, 30, 35, 31, 30, 28, 0, 2, 12, 13, 13, 21, 26),
+  ord=list(1, 1, 1, 1, 1, 2, 2, 2, 2, 2, -1, -1, -1, -1, -1, -2, -2, -2, -2, -2),
+  col=list("violet", "dark green", "light green", "dark yellow", "light yellow", 
+           "violet", "dark green", "light green", "dark yellow", "light yellow",
+           "violet", "dark green", "light green", "dark yellow", "light yellow",
+           "violet", "dark green", "light green", "dark yellow", "light yellow",)
+)
