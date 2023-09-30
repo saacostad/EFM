48492a8155d8a9611c783033474a5729fd845f87
morumotto <saacostad@unal.edu.co>
2023-09-30 16:52:33 -0500

    Added analysis output

--- Frank-Hertz.R
+++ Frank-Hertz.R
@@ -53,17 +53,48 @@ findMaxValues <- function(data, th)
 # Bloque para corroborar los picos que se buscan
 # Para valores de alta temperatura, conviene usar un treshold = 15
 # Para valores de baja temperatura, conviene usar un treshold = 5
-for (i in 1:15)
-{
-  max_D = findMaxValues(low_temp_D[i], 5)
-  
-  plot(low_temp_D[[i]])
-  for (maxval in max_D)
-  {
-    print(maxval[[1]])
-    abline(v = maxval[[1]])
+# for (i in 1:15)
+# {
+#   max_D = findMaxValues(low_temp_D[i], 5)
+#   
+#   plot(low_temp_D[[i]])
+#   for (maxval in max_D)
+#   {
+#     print(maxval[[1]])
+#     abline(v = maxval[[1]])
+#   }
+# }
+
+
+# Se crean las listas con los valores máximos encontrados
+low_temp_maxValues = lapply(low_temp_D, function(data) findMaxValues(list(data), 5))
+high_temp_maxValues = lapply(high_temp_D, function(data) findMaxValues(list(data), 15))
+
+# |------------------------|
+# | HALLAR EL PRIMER PICO  |
+# |------------------------|
+
+first_peak_D = unlist(lapply(low_temp_maxValues, function(data) data[[1]][[1]]))
+
+print(paste("Promedio para el valor del primer pico de voltaje: ", mean(first_peak_D)))
+print(paste("Desviación para el valor del primer pico de voltaje: ", sd(first_peak_D)))
+
+
+
+# |-------------------------------------------------------------|
+# | FUNCIÓN PARA HALLAR EL ESPACIAMIENTO ENTRE PICOS DE VOLTAJE |
+# |-------------------------------------------------------------|
+peaks_difference = c()
+for (measure in high_temp_maxValues) {
+  for (peak in 4:2){
+    peaks_difference <- append(peaks_difference, measure[[peak]][1] - measure[[peak - 1]][1])
   }
 }
+peaks_difference = unlist(peaks_difference)
+
+print(paste("Diferencia promedio entre picos en temperatura alta: ", mean(peaks_difference)))
+print(paste("Desv. Est de la diferencia entre picos en temperatura alta: ", sd(peaks_difference)))
+
 
 
 
