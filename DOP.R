9560bedae60f8a8021de30a4511ba989f1a842d8
morumotto <saacostad@unal.edu.co>
2023-10-07 11:50:09 -0500

    Created DOP repository

---
+++ DOP.R
@@ -0,0 +1,13 @@
+# |-------------------|
+# | CREACIÓN DE DATOS |
+# |-------------------|
+# Como los datos conseguidos son, en sí, imágenes, el procesamiento de estas fue hecho con
+# hecho con ayuda de tracker, de donde se consiguen los siguientes datos.
+
+voltage = c(2600, )
+
+firstRingPos = c(0.0165, )
+firstRingThickness = c(0.004626, )
+
+secondRingPos = c(0.02779, )
+secondRingThickness = c(0.00574, )
