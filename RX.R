12bb658b2a7fc56e5037812a857bfc23e2a097e7
morumotto <saacostad@unal.edu.co>
2023-10-30 14:40:46 -0500

    Added 4 more results, need to add final result

--- RX.R
+++ RX.R
@@ -44,6 +44,7 @@ meanExpData <- function(rval, eval)
   return(c(meanval, errval))
 }
 
+propProdErrors <- function(res, rval, eval){return(abs(res * sqrt(sum((eval/ rval)^2))))}
 
 # |-----------------------------------------------|
 # |               PRIMER RESULTADO                |
@@ -57,9 +58,11 @@ td = 5.6402e-10 # Constante 2d para el monocristal de sal
 # difracción y el orden
 BraggCondition <- function(angle, order){
   relVal = td * sin(angle * pi / 180) / order
-  error = 9.84399e-13 / order
+  error = td * cos(angle * pi / 180) * (0.1 * pi / 180) / order
   return(c(relVal, error))
 }
+
+
 
 
 # Utilizando los resultados del espectro completo, hallamos la longitud de onda para Ka y Kb
@@ -88,21 +91,134 @@ Kb = meanExpData(KbEst, KbErrors)
 rKa = 7.108e-11
 rKb = 6.3095e-11
 
-print(paste("Estimación Ka: ", Ka[1], " pm ", Ka[2]))
+print("ESTIMAR LA LONGITUD DE ONDA Y ENERGÍA CARACTERÍSTICA DE LOS PICOS: COMPROBAR BRAGG")
+print(" ")
+print(paste("Estimación Ka: ", signif(Ka[1], 3), " pm ", signif(Ka[2], 1)))
 print(paste("Error relativo Ka: ", 1 - (Ka[1] / rKa)))
 
-print(paste("Estimación Kb: ", Kb[1], " pm ", Kb[2]))
+print(paste("Estimación Kb: ", signif(Kb[1], 3), " pm ", signif(Kb[2], 3)))
 print(paste("Error relativo Kb: ", 1 - (Kb[1] / rKb)))
 
 
 
-# |-----------------------------------------------------|
-# |                 SEGUNDO RESULTADO                   |
-# | INVARIABILIDAD DE ENERGÍA PARA INTENSIDAD VARIABLE  |
-# |-----------------------------------------------------|
+
+
+# |-----------------------------|
+# | Se encuentran las energías  |
+# |-----------------------------|
+
+hc = 1.23984193e-6   # Valor ed HC en eV
+
+eKa = hc / Ka[1]
+eKaError = propProdErrors(eKa, c(Ka[1]), c(Ka[2]))
+
+eKb = hc / Kb[1]
+eKbError = propProdErrors(eKb, c(Kb[1]), c(Kb[2]))
+
+print(paste("Energía característica para Ka: ", signif(eKa, 3), " pm ", signif(eKaError, 3)))
+print(paste("Energía característica para Kb: ", signif(eKb, 3), " pm ", signif(eKbError, 3)))
+
+
+# |-------------------------------------|
+# |         SEGUNDO RESULTADO           |
+# | ENERGÍA LÍMITE PARA I Y U VARIABLE  |
+# |-------------------------------------|
+
+findAccRad <- function(data, th)
+{
+  maxVal = max(data[,2][1:th])
+  
+  for(i in 70:1){
+    if(data[i,2] < maxVal){
+      return(data[i, 1])
+    }
+  }
+}
+
+print(" ")
+print(" ")
+print(" ")
+print("ENERGÍA LÍMITE PARA I Y U VAR: MOSTRAR QUE I VAR NO TIENE CAMBIO DE ENERGÍA MIENTRAS QUE U VAR SÍ LO TIENE")
+print(" ")
+
+IvarLim = unlist(lapply(IVarData, function(data) findAccRad(data, 13)))
+print("Valores de b límite para I variable: ")
+print(IvarLim)
+print(paste("Coeficiente de variación de los datos: ", sd(IvarLim) / mean(IvarLim) * 100))
+            
+
+
+print(" ")
+UvarLim = unlist(lapply(1:5, function(i) findAccRad(UVarData[[i]], trunc(55 / i))))
+print("Valores de b límite para U variable: ")
+print(UvarLim)
+print(paste("Coeficiente de variación de los datos: ", sd(UvarLim) / mean(UvarLim) * 100))
+
+
+# |-------------------------------------------|
+# |           TERCER RESULTADO                |
+# | ESTATICIDAD DE LOS PICOS CARACTERÍSTICOS  |
+# |-------------------------------------------|
+
+IVKa = unlist(lapply(IVpeaks, function(l) l[[1]][[1]]))
+IVKb = unlist(lapply(IVpeaks, function(l) l[[2]][[1]]))
+
+UVKa = unlist(lapply(UVpeaks[3:5], function(l) l[[1]][[1]]))
+UVKb = append(unlist(lapply(UVpeaks[3:5], function(l) l[[2]][[1]])), UVpeaks[[2]][[1]][[1]])
+
+print(" ")
+print(" ")
+print(" ")
+
+print("VALORES DE B PARA LOS DIFERENTES PICOS: MOSTRAR QUE SON LOS MISMOS")
+print(" ")
+
+print("Valores de b para Ka encontrados para I variable: ")
+print(IVKa)
+print(paste("Coeficiente de variación de los datos: ", sd(IVKa) / mean(IVKa) * 100))
+
+print(" ")
+print("Valores de b para Kb encontrados para I variable: ")
+print(IVKb)
+print(paste("Coeficiente de variación de los datos: ", sd(IVKb) / mean(IVKb) * 100))
+
+
+print(" ")
+print(" ")
+print("Valores de b para Ka encontrados para U variable: ")
+print(UVKa)
+print(paste("Coeficiente de variación de los datos: ", sd(UVKa) / mean(UVKa) * 100))
+
+print(" ")
+print("Valores de b para Kb encontrados para U variable: ")
+print(UVKb)
+print(paste("Coeficiente de variación de los datos: ", sd(UVKb) / mean(UVKb) * 100))
 
 
 
+# |---------------------------------------|
+# |           CUARTO RESULTADO            |
+# | MAXIMOS DE LOS PICOS PARA I VARIABLE  |
+# |---------------------------------------|
+
+par(mar=c(5,6,4,1)+.1)
+plot(c(0.4, 0.6, 0.8, 1), unlist((lapply(IVpeaks, function(l) l[[2]][[2]]))),
+     lwd = 2, 
+     col = 1,
+     ylim = c(700, 2700),
+     xlab = "I [mA]",
+     ylab = "R [1/s]",
+     cex.lab = 2.5,
+     cex.axis = 2,
+     cex = 1.5,)
+points(c(0.4, 0.6, 0.8, 1), unlist((lapply(IVpeaks, function(l) l[[1]][[2]]))),
+       lwd = 2, 
+       col = 2,
+       pch = 2,
+       cex = 1.5)
+legend("topleft", legend=c(
+  unlist(lapply(c("Kb", "Ka"), function(val) paste("U = ", val, " [mA]")))),
+  col=c(2:1), cex = 1.35, lwd = 2)
 
 # |-------------------|
 # | APARTADO GRÁFICO  |
