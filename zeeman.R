0070f3fb15f2e16c5f7a08c37e2396b5b6220ca2
morumotto <92110089+saacostad@users.noreply.github.com>
2023-11-17 21:58:13 -0500

    milagrosamente dio un buen resultado

--- zeeman.R
+++ zeeman.R
@@ -1,5 +1,5 @@
-DLAMBDA = c(64-16, 64-15, 65-18, 67-19, 70-22, 71-23, 66-18, 67-19, 66-18, 67-19, 70-22, 67-19)
-dlambda = c(16-8, 64-55, 65-55, 67-57, 30-22, 32-23, 28-18, 29-19, 28-18, 28-19, 33-22, 29-19)
+DLAMBDA = c(52.75, 50, 44.35, 54.61, 50.24, 47.89, 55.28, 49.1, 41.65, 40.74, 46.18, 54.16)
+dlambda = c(8.01, 10, 7.48, 11.48, 13.26, 7.57, 11.33, 11.01, 13.62, 11.41, 10.46, 12.5)
 current = c(5, 5, 5.5, 5.5, 6, 6, 6.5, 6.5, 7, 7, 7.5, 7.5)
 
 lambda = 0.02e-9      # Longitud de onda de la linea roja del cadmio
@@ -9,3 +9,34 @@ dlambdaConstant = (lambda^2) / ( (2 * 4.04e-3) * sqrt( (1.4567)^2 - lambda) )
 lambdaRatio <- function(D, d){
   return(d * dlambdaConstant / D)
 }
+
+lambdaRatios = unlist(lapply(1:12, function(i) {lambdaRatio(DLAMBDA[i], dlambda[i])}))
+
+newLambdaRatios = unlist(lapply( ((1:6)*2 - 1), function(i) mean(c(lambdaRatios[i], lambdaRatios[i + 1]))))
+ccurrent = c(5, 5.5, 6, 6.5, 7, 7.5)
+plot(ccurrent, newLambdaRatios)
+
+
+hc = 1.986e-25
+mb = 9.27e-24
+
+DeltaE = function(DL){
+  return(hc * DL / lambda^2)
+}
+
+DeltaEs = unlist(lapply(newLambdaRatios, DeltaE))
+
+B = c(351e-3, 378e-3, 401e-3, 417e-3, 443e-3)
+
+plot(B, DeltaEs[c(1, 2, 3, 4, 6)])
+
+
+dat = data.frame(list(B, DeltaEs[c(1, 2, 3, 4, 6)]))
+colnames(dat) = c("B", "dE")
+
+
+plot(dat)
+lm(dE ~ B, dat)
+
+
+1 - 9.922e-24 / mb
